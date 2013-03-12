e2fa17babca78ca02bbc572ce3a7e8b9cfa63bf9
Tony Heupel <tonyheupel@gmail.com>
2013-03-12 13:58:05 -0700

    finish telegram tests

--- spec/telegrams/telegram.rb
+++ spec/telegrams/telegram.rb
@@ -5,21 +5,31 @@ require_relative '../../lib/telegrams/telegram'
 describe Telegram do
   before do
     @telegram = Telegram.new
+    @type_value = 0x77
+    @telegram.instance_eval("@type = #{@type_value}")
   end
 
   describe "when accessing the type property" do
-    before do
-      @type_value = 0x77
-      @telegram.instance_eval("@type = #{@type_value}")
-    end
-
     it "allows read access" do
       @telegram.type.must_equal @type_value
     end
 
     it "does not allow write access" do
-      -> { @telegram.type = "biz buzz" }.must_raise(NoMethodError)
+      -> { @telegram.type = "biz buzz" }.must_raise NoMethodError
       @telegram.type.must_equal @type_value
+    end
+  end
+
+  describe "when getting the telegram as bytes" do
+    it "must return the type value as the first element of the array" do
+      @telegram.as_bytes[0].must_equal @type_value
+    end
+  end
+
+
+  describe "when working with a telegram" do
+    it "specifies a maximum size of 64 bytes" do
+      @telegram.max_size_in_bytes.must_equal 64
     end
   end
 
