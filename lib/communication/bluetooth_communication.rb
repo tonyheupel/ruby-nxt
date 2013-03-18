9206efbd100ac4f81296cfbe5bba55c73f35e4b1
Tony Heupel <tonyheupel@gmail.com>
2013-03-18 08:14:19 -0700

    bluetooth communication constructor

--- lib/communication/bluetooth_communication.rb
+++ lib/communication/bluetooth_communication.rb
@@ -1,4 +1,9 @@
+require_relative './serial_port_profile'
+
 class BluetoothCommunication
-  def initialize(profile_type=SerialPortProfile)
+  def initialize(device, serial_port_profile=nil)
+    raise ArgumentError, "device must be non-nil" if device.nil?
+
+    @profile = serial_port_profile || SerialPortProfile.new(device)
   end
 end
--- spec/communication/bluetooth_communication_spec.rb
+++ spec/communication/bluetooth_communication_spec.rb
@@ -1,3 +1,23 @@
 require 'minitest/autorun'
 
 require_relative '../../lib/communication/bluetooth_communication.rb'
+
+class BluetoothCommunication
+  attr_accessor :profile
+end
+
+describe BluetoothCommunication do
+  describe "when creating a new bluetooth communication object" do
+    it "should raise an ArgumentError if device is not set" do
+      -> { BluetoothCommunication.new }.must_raise ArgumentError
+    end
+
+    it "should raise an ArgumentError if device is nil" do
+      -> { BluetoothCommunication.new(nil, Object.new) }.must_raise ArgumentError
+    end
+
+    it "should default to using a SerialPortProfile for its profile" do
+      BluetoothCommunication.new('dev').profile.must_be_instance_of SerialPortProfile
+    end
+  end
+end
