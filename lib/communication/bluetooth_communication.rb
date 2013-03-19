6527354802b344bc434b7047975998d948553546
Tony Heupel <tonyheupel@gmail.com>
2013-03-18 19:47:42 -0700

    add notes on bit shifting

--- lib/communication/bluetooth_communication.rb
+++ lib/communication/bluetooth_communication.rb
@@ -6,4 +6,39 @@ class BluetoothCommunication
 
     @profile = serial_port_profile || SerialPortProfile.new(device)
   end
+
+  def connect
+    @profile.connect
+  end
+
+  def disconnect
+    @profile.disconnect
+  end
+
+  def send(message)
+    # TODO: Determine whether to wait for response or not
+    package = create_bluetooth_data_package(message)
+    @profile.send_data_package(package)
+  end
+
+
+  private
+  def create_bluetooth_data_package(message)
+    bytes = message.as_bytes
+    [calculate_length_lsb(bytes), calculate_length_msb(bytes)].concat(bytes)
+  end
+
+  def calculate_length_lsb(bytes)
+    # Filter to only get the least significant byte's worth (and lose anything higher)
+    bytes.length & 0xff
+    # NOTE: Since the max message length is 64 bytes, the Least Significant Byte
+    #       should always be the actual length since it fits in one byte
+  end
+
+  def calculate_length_msb(bytes)
+    # Bit shift right by a byte, and then filter to only get one byte's worth
+    (bytes.length >> 8) & 0xff
+    # NOTE: Since the max message length is 64 bytes, the Most Significant Byte
+    #       should always be zero since the length fits in one byte (LSB)
+  end
 end
--- spec/communication/bluetooth_communication_spec.rb
+++ spec/communication/bluetooth_communication_spec.rb
@@ -8,16 +8,55 @@ end
 
 describe BluetoothCommunication do
   describe "when creating a new bluetooth communication object" do
-    it "should raise an ArgumentError if device is not set" do
+    it "must raise an ArgumentError if device is not set" do
       -> { BluetoothCommunication.new }.must_raise ArgumentError
     end
 
-    it "should raise an ArgumentError if device is nil" do
+    it "must raise an ArgumentError if device is nil" do
       -> { BluetoothCommunication.new(nil, Object.new) }.must_raise ArgumentError
     end
 
-    it "should default to using a SerialPortProfile for its profile" do
+    it "must default to using a SerialPortProfile for its profile" do
       BluetoothCommunication.new('dev').profile.must_be_instance_of SerialPortProfile
+    end
+
+    it "must use the profile passed in if provided" do
+      fake_profile = Object.new
+      BluetoothCommunication.new('dev', fake_profile).profile.must_be(:==, fake_profile)
+    end
+  end
+
+  describe "when connecting over bluetooth" do
+    it "should use connect using the serial profile" do
+      mock_profile = MiniTest::Mock.new.expect(:connect, nil)
+      BluetoothCommunication.new('dev', mock_profile).connect
+
+      mock_profile.verify
+    end
+
+    it "should disconnect using the serial profile" do
+      mock_profile = MiniTest::Mock.new.expect(:disconnect, nil)
+      BluetoothCommunication.new('dev', mock_profile).disconnect
+
+      mock_profile.verify
+    end
+  end
+
+  describe "when sending a message over bluetooth" do
+    before do
+      @mock_profile = MiniTest::Mock.new
+      @communication = BluetoothCommunication.new('dev', @mock_profile)
+    end
+
+
+    it "must call as_bytes on the telegram being passed in" do
+      message = MiniTest::Mock.new.expect(:as_bytes, [0,1,2])
+      @mock_profile.expect(:send_data_package, nil, [[3,0,0,1,2]])
+
+      @communication.send(message)
+
+      message.verify
+      @mock_profile.verify
     end
   end
 end
