655342751639ae96d307112747f018f67a785705
Tony Heupel <tonyheupel@gmail.com>
2013-04-14 12:03:46 -0700

    make GetCurrentProgramNameReply DRY

--- lib/telegrams/commands/direct/replies/get_current_program_name_reply.rb
+++ lib/telegrams/commands/direct/replies/get_current_program_name_reply.rb
@@ -1,6 +1,9 @@
 require_relative '../../direct_command_reply'
+require_relative '../../message_translator'
 
 class GetCurrentProgramNameReply < DirectCommandReply
+  include MessageTranslator
+
   def initialize(bytes)
     super
     raise RuntimeError, "reply must be for 0x11, but was #{command}" unless command == 0x11
@@ -17,7 +20,7 @@ class GetCurrentProgramNameReply < DirectCommandReply
 
   private
   def parse_message_bytes_into_filename
-    message_bytes.pack('C*').strip
+    string_from_bytes(message_bytes)
   end
 
 end
