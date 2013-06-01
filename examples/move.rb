daa9b275675e1c1fb768a8ed3829ec912c158f6b
Tony Heupel <tonyheupel@gmail.com>
2013-06-01 11:34:36 -0700

    small cleanup of examples

--- examples/move.rb
+++ examples/move.rb
@@ -5,17 +5,6 @@ nxt = NXT.new('/dev/tty.NXT-DevB')
 puts "Connecting..."
 nxt.connect
 
-puts "Moving what is typically the left wheel one rotation and then brake..."
-state = OutputState.new :port => :c,
-                        :power => 55,
-                        :mode_flags => OutputModeFlags.MOTORON | OutputModeFlags.BRAKE,
-                        :regulation_mode => :motor_speed,
-                        :run_state => :running,
-                        :tacho_limit => 360  # degrees = 1 rotation
-
-#puts nxt.set_output_state state
-#sleep(3)
-
 puts "Driving forward two rotations, ramping up to 75 power and then coasting..."
 state = OutputState.new :port => :c,
                         :power => 75,
@@ -45,6 +34,10 @@ state = OutputState.new :port => :all,
                         :regulation_mode => :idle,
                         :run_state => :idle,
                         :tacho_limit => 0 # unlimited
+
+nxt.set_output_state state
+sleep(1)
+nxt.reset_motor_position
 
 puts "Done moving."
 nxt.disconnect
